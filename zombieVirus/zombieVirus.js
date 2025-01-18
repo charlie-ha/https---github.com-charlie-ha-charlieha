5974630c834a9fae1870076f8d8efc2dd8668025
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2025-01-18 12:12:51 -0500

    commt

---
+++ zombieVirus/zombieVirus.js
@@ -0,0 +1,6 @@
+// JavaScript function to jump to specific time
+function jumpTo(seconds) {
+  const video = document.getElementById("winCondition");
+  video.currentTime = seconds;
+  video.play();
+}
