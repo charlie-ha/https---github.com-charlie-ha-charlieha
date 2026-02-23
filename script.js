1a13364517cf85fbe4f746e40c43722f94c3c4b3
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2026-02-22 19:03:40 -0500

    bottom nav

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-
 function menu() {
-    var x = document.getElementById("navigation");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-        }
+  var x = document.getElementById("navigation");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
+// Load Bottom CTA
+fetch("bottomNav.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("bottom-cta-placeholder").innerHTML = data;
+  });
+// animate showing CTA bottom of the page
+window.addEventListener("scroll", () => {
+  const cta = document.querySelector(".bottom-cta");
+  if (!cta) return;
+
+  const triggerPoint = document.body.offsetHeight - window.innerHeight - 200;
+
+  if (window.scrollY > triggerPoint) {
+    cta.classList.add("show");
+  }
+});
